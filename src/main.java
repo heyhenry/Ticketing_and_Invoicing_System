449227d1acce3a4ae5a4f9ef63b969e211e2998c
Nampa <henryyy3@hotmail.com>
2020-07-29 12:21:06 +0800

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        // account related
+        int accountNumber;
+
+        // customer related
+        String name;
+        String email;
+        int mobile;
+        String home;
+
+        // card related
+        int creditCardNo;
+        int cardExpiryDate;
+
+        // sector related
+        int date;
+        int startSector;
+        int endSector;
+
+    }
+
+}
